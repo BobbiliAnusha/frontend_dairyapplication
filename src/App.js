d07a2ee09af418f1cc90e3b732aa1aa3220a8869
BobbiliAnusha <106606897+BobbiliAnusha@users.noreply.github.com>
2022-06-07 14:54:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import SignUpComponent from './components/SignUp';
+import LoginComponent from './components/Login';
+import Home from './components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DairyComponent from './components/Dairy';
+import UserCredentialsComponent from './components/UserCredentials';
+import NewComponent from './components/NewComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+       <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginComponent />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUpComponent />} />
+            <Route path='/usercredentials/:userid' element={<UserCredentialsComponent/>}/>
+            <Route path='/users/:userid' element={<DairyComponent/>}/>
+            <Route path='/userdairy' element={<NewComponent/>}/>
+            </Routes>
+        </BrowserRouter>
+     </div>
+  );
+}
+
+export default App;
+
